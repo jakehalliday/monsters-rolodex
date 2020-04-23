df3b6e2969439166d2d47b167983c6c7dc208f42
Jake Halliday <20205690+jakehalliday@users.noreply.github.com>
2020-04-23 17:41:13 +0100

    Updates

---
+++ precache-manifest.092eb44e822fac1e35bd6dc1b3f6344e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2df6ddeccc187da73ca85ddc31e7936",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "86a69088d5bead0c19db",
+    "url": "/monsters-rolodex/static/css/main.0cc0b5e2.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a69088d5bead0c19db",
+    "url": "/monsters-rolodex/static/js/main.a265e16a.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
